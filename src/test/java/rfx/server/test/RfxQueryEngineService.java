ca0c8f16d3702b8e7728276726050afeef28e4b9
trieu <trieunt@fpt.com.vn>
2015-10-06 17:11:26 +0700

    IRIS demo

--- src/test/java/rfx/server/test/RfxQueryEngineService.java
+++ src/test/java/rfx/server/test/RfxQueryEngineService.java
@@ -1,8 +1,10 @@
 package rfx.server.test;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -22,6 +24,8 @@ import rfx.server.netty.NettyServerUtil;
 
 import com.corundumstudio.socketio.listener.*;
 import com.corundumstudio.socketio.*;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 
 public class RfxQueryEngineService {
@@ -46,9 +50,51 @@ public class RfxQueryEngineService {
 			public void run() {
 				Query q = queries.poll();
 				if(q != null){
+					String qs = q.getQuery();
+					
 					Map<String, Object> map = new HashMap<>();
 					map.put("ok", 1);
-					map.put("list", Arrays.asList("abc","def"));
+					
+					if(qs.contains("One Direction")){
+						map.put("nhacso", "<iframe src='http://nhacso.net/embed/song/XFhRU0NabA==' width='400' height='140' frameborder='0'></iframe>");
+					}
+					else if(qs.contains("report of AdsPlay")){
+						map.put("reportFields", Arrays.asList("date","impressions", "clicks", "trueviews"));
+						
+						List<Map<String,Object>> reportObj = new ArrayList<Map<String,Object>>();						
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("date", "2015-10-04").put("impression", 14358).put("click",433).put("trueview",12845)
+								.build());						
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("date", "2015-10-05").put("impression", 13358).put("click",433).put("trueview",12845)
+								.build());
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("date", "2015-10-06").put("impression", 12358).put("click",433).put("trueview",12845)
+								.build());
+						
+						map.put("reportAdsData", reportObj);						
+					}
+					else if(qs.contains("report of Device Type")){
+						List<Map<String,Object>> reportObj = new ArrayList<Map<String,Object>>();
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("label", "PC").put("value", 323001)
+								.build());			
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("label", "SmartTV").put("value", 144892)
+								.build());			
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("label", "Tablet").put("value", 7034)
+								.build());
+						reportObj.add(ImmutableMap.<String, Object>builder()
+								.put("label", "Mobile").put("value", 17489)
+								.build());
+						
+						map.put("reportDeviceData", reportObj);
+					}
+					else {
+						map.put("msg", Arrays.asList("welcome Mr.Trieu to IRIS System"));	
+					}
+					
 					String jsonData = new Gson().toJson(map);
 					// broadcast messages to the sender
 					server.getClient(q.getUuid()).sendEvent("queryResult",new Result(q.getSenderId(), jsonData ));					
